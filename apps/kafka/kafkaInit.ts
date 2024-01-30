6c4020da7842258190f9e129b6a7a9929dd781ab
Kevon Eaglin <keaglin@users.noreply.github.com>
2024-01-30 02:01:18 -0500

    Update kafkaInit.ts

--- apps/kafka/kafkaInit.ts
+++ apps/kafka/kafkaInit.ts
@@ -2,7 +2,7 @@ import { Kafka } from 'kafkajs'
 
 const kafka = new Kafka({
   clientId: 'scriptsifter-web-client',
-  brokers: ['localhost:9092']
+  brokers: ['localhost:9092', 'kafka:9092'],
 });
 
 const admin = kafka.admin();
