f5e745ebcecef776f58497d7e843e8d64b5e49a8
Kevon Eaglin <keaglin@users.noreply.github.com>
2024-01-30 03:47:23 -0500

    fix kafka config

--- apps/web/src/services/kafka.ts
+++ apps/web/src/services/kafka.ts
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs";
 
 export const kafka = new Kafka({
   clientId: "scriptsifter-web-client",
-  brokers: ["localhost:9092"],
+  brokers: ["localhost:9092", 'kafka:9092'],
 });
 
 const producer = kafka.producer();
