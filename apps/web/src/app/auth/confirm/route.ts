d5d796064ecb95bdbab8a2ff58df00be7489b763
Kevon Eaglin <keaglin@users.noreply.github.com>
2024-01-30 23:13:28 -0500

    follow supabase auth guide

---
+++ apps/web/src/app/auth/confirm/route.ts
@@ -0,0 +1,35 @@
+
+import { createClient } from '@/services/supabase/actions'
+import { type EmailOtpType } from '@supabase/supabase-js'
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url);
+  const searchParams = new URLSearchParams(url.search);
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type') as EmailOtpType | null
+  const next = searchParams.get('next') ?? '/'
+  const redirectTo = request.nextUrl.clone()
+  redirectTo.pathname = next
+
+  if (token_hash && type) {
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    })
+
+    if (!error) {
+      return NextResponse.redirect(redirectTo)
+    }
+
+    console.error('There was a problem authenticating this user.', error)
+  }
+
+  // return the user to an error page with some instructions
+  redirectTo.pathname = '/auth/auth-code-error'
+  return NextResponse.redirect(redirectTo)
+}
