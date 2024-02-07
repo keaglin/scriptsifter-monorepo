ed31186198976491b514a656029da5f487426a37
Kevon Eaglin <keaglin@users.noreply.github.com>
2024-02-07 00:42:08 -0500

    cleanup pass 1
    move uppy/kafka files to upload dirs

--- apps/web/src/app/api/upload/route.ts
+++ apps/web/src/app/api/upload/route.ts
@@ -1,50 +1,35 @@
-import { NextRequest, NextResponse } from 'next/server'
+// import { NextRequest, Response } from 'next/server'
 import { cookies } from 'next/headers'
 import { createClient } from '@/services/supabase/server'
 import { producer } from '@/services/kafka'
 import { storageLimit } from '@/lib/utils'
 import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
 
-export async function POST(request: NextRequest) {
-  const data = await request.formData()
-  const file: File | null = data.get('file') as unknown as File
+export async function POST(request: Request) {
+  const data = await request.json()
+  console.log('data', data)
+  const { fileUploadLocation, token: bearerToken } = data
+  // const file: File | null = data.get('file') as unknown as File
   const cookieStore = cookies()
   const supabase = createClient(cookieStore)
   const token = JSON.parse(cookieStore.get('sb-uhpcxcyzuhmshpzfoxgc-auth-token')?.value ?? '{}')?.access_token
 
-  // console.log('token', token)
-
-  if (!file) {
-    return NextResponse.json({ success: false })
-  }
+  if (token !== bearerToken) return Response.json({ success: false })
 
   const { data: { session } } = await supabase.auth.getSession();
-  if (!session) return NextResponse.json({ success: false })
+  if (!session) return Response.json({ success: false })
 
   const { data: { user } } = await supabase.auth.getUser();
   const userId = user?.id
 
-  if (file.size > storageLimit) {
-    console.log('File too large')
-    return NextResponse.json({ success: false })
-  }
-
-  const { data: storageData, error } = await supabase.storage.from('transcripts').upload(`${userId}/${Date.now()}-${file.name}`, file)
-
-  if (error) {
-    console.log('Error uploading file:', error.message)
-    return NextResponse.json({ success: false })
-  }
-
-  console.log('File uploaded successfully:', storageData.path)
-
   // save a new transcript to the database and put the bucket path in a column
   // TODO: fix types
   // @ts-ignore
-  const { data: createData, error: createError } = await supabase.from('transcripts').insert({ user_id: userId, filepath: storageData?.path }).select('id')
+  const { data: createData, error: createError } = await supabase.from('transcripts').insert({ user_id: userId, filepath: fileUploadLocation }).select('id')
   if (createError) {
     console.log('Error creating transcript:', createError.message)
-    return NextResponse.json({ success: false })
+    return Response.json({ success: false })
   }
 
   console.log('Transcript created successfully:', createData)
@@ -54,7 +39,7 @@ export async function POST(request: NextRequest) {
   const payload = {
     // TODO:
     // transactionid, -- uuid or cid for the transaction itself; for traceability/observability
-    location: storageData?.path,
+    location: fileUploadLocation,
     token,
     transcriptId
   }
@@ -66,6 +51,7 @@ export async function POST(request: NextRequest) {
     console.log('Error publishing to Kafka:', err)
   }
 
-  // return NextResponse.json({ success: true })
+  // return Response.json({ success: true })
+  revalidatePath('/transcripts')
   redirect(`/transcripts/${transcriptId}`)
 }
