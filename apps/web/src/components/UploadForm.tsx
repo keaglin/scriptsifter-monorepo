b56afa8c79bab3611630f3e65bc701facccefe92
Kevon Eaglin <keaglin@users.noreply.github.com>
2024-02-07 10:50:29 -0500

    Update UploadForm.tsx

--- apps/web/src/components/UploadForm.tsx
+++ apps/web/src/components/UploadForm.tsx
@@ -90,7 +90,7 @@ export default function UploadForm({ token, userId }: { token: string, userId: s
       setFileUploadLocation('')
       filename.current = ''
 
-      await fetch('/api/kafka', {
+      await fetch('/api/upload', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
